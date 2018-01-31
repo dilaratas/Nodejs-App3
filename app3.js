864cf11cf513460046259084ebe924363c58096a
dilaratas <35151606+dilaratas@users.noreply.github.com>
2018-02-01 01:06:00 +0200

    express calismalar

---
+++ app3.js
@@ -0,0 +1,34 @@
+
+
+var fs=require('fs');
+
+var express = require('express');
+var app=express();
+app.use('/public',express.static(path.join(__dirname,'public')));
+
+app.get('/', function(req,res){
+
+    fs.readFile('index.html',function(err,data){
+        res.write(data);
+        res.end('mesaj bitti');
+        console.log('homeController');
+    });
+
+});
+
+
+
+app.get('/login', function(req,res){
+
+    fs.readFile('login.html',function(err,data){
+        res.write(data);
+        res.write('mesaj bitti');
+        console.log('loginController');
+    });
+   
+
+});
+
+
+
+app.listen(8000);
